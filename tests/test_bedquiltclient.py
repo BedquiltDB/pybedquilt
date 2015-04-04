0fceec592db2206d508b602a3f9870aa1850e0d5
Shane Kilkelly <shane@kilkelly.me>
2015-04-04 11:39:32 +0100

    add tests

---
+++ tests/test_bedquiltclient.py
@@ -0,0 +1,8 @@
+import pybedquilt
+import testutils
+
+
+class TestBedquiltClient(testutils.BedquiltTestCase):
+
+    def test_something(self):
+        pass
---
+++ tests/testutils.py
@@ -0,0 +1,45 @@
+import psycopg2
+import os
+import getpass
+import unittest
+import json
+
+
+# CREATE DATABASE bedquilt_test
+#   WITH OWNER = {{owner}}
+#        ENCODING = 'UTF8'
+#        TABLESPACE = pg_default
+#        LC_COLLATE = 'en_GB.UTF-8'
+#        LC_CTYPE = 'en_GB.UTF-8'
+#        CONNECTION LIMIT = -1;
+
+
+def get_pg_connection():
+    """
+    Get a connection to localhost/bedquilt_test,
+    as the current user.
+    """
+    return psycopg2.connect(
+        database='bedquilt_test',
+        user=getpass.getuser()
+    )
+PG_CONN  = get_pg_connection()
+
+
+def clean_database(conn):
+    cur = conn.cursor()
+    cur.execute("select bq_list_collections();")
+    result = cur.fetchall()
+    if result is not None:
+        for collection in result:
+            cur.execute(
+                "select bq_delete_collection('{}')".format(collection[0]))
+
+    conn.commit()
+
+class BedquiltTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.conn = PG_CONN
+        self.cur = self.conn.cursor()
+        clean_database(self.conn)
