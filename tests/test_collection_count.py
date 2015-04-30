98efaf45fbc32565c79b9cd792c3e2780231b5b2
Shane Kilkelly <shane@kilkelly.me>
2015-04-30 22:18:30 +0100

    More tests

--- tests/test_collection_count.py
+++ tests/test_collection_count.py
@@ -18,3 +18,20 @@ class TestCollectionCount(testutils.BedquiltTestCase):
 
         coll = client.collection('things')
         self.assertEqual(coll.count(), 0)
+
+    def test_simple_scenario(self):
+        client = self._get_test_client()
+        coll = client.collection('things')
+
+        coll.insert({'_id': 'spanner', 'type': 'tool'})
+        coll.insert({'_id': 'hammer', 'type': 'tool'})
+        coll.insert({'_id': 'screwdriver', 'type': 'tool'})
+
+        coll.insert({'_id': 'crow', 'type': 'bird'})
+        coll.insert({'_id': 'sparrow', 'type': 'bird'})
+        coll.insert({'_id': 'weasel', 'type': 'animal'})
+
+        self.assertEqual(coll.count(), 6)
+        self.assertEqual(coll.count({'type': 'tool'}), 3)
+        self.assertEqual(coll.count({'type': 'bird'}), 2)
+        self.assertEqual(coll.count({'_id': 'crow'}), 1)
