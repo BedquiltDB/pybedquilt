31a2222ff14e8968f11e0f705e814453a632c0fd
Shane Kilkelly <shane@kilkelly.me>
2015-06-12 19:39:04 +0100

    Test removing the $required constraint.

--- tests/test_collection_constraints.py
+++ tests/test_collection_constraints.py
@@ -49,3 +49,58 @@ class TestAddRequiredConstraint(testutils.BedquiltTestCase):
             }),
             'paul@example.com'
         )
+
+    def test_removing_required_constraint(self):
+        client = self._get_test_client()
+        coll = client['people']
+
+        # add the constraint
+        result = coll.add_constraints({
+            'name': {'$required': 1}
+        })
+        self.assertEqual(result, True)
+
+        # reject bad doc
+        with self.assertRaises(psycopg2.IntegrityError):
+            coll.insert({
+                '_id': 'paul@example.com',
+                'age': 20
+            })
+        self.conn.rollback()
+
+        # accept doc with name present and set to a string value
+        self.assertEquals(
+            coll.save({
+                '_id': 'paul@example.com',
+                'name': 'Paul',
+                'age': 20
+            }),
+            'paul@example.com'
+        )
+
+        # clear the collection
+        coll.remove({})
+
+        # remove constraints
+        coll.remove_constraints({
+            'name': {'$required': 1}
+        })
+
+        # accept previously rejected doc
+        self.assertEquals(
+            coll.save({
+                '_id': 'paul@example.com',
+                'age': 20
+            }),
+            'paul@example.com'
+        )
+
+        # accept doc with name present and set to a string value
+        self.assertEquals(
+            coll.save({
+                '_id': 'paul@example.com',
+                'name': 'Paul',
+                'age': 20
+            }),
+            'paul@example.com'
+        )
