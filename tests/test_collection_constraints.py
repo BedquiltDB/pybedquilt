fd2645ec7dcd7bee0a7045b1f691ccf975f5b21d
Shane Kilkelly <shane@kilkelly.me>
2015-06-13 09:26:36 +0100

    Begin abstracting over testing constraints.

--- tests/test_collection_constraints.py
+++ tests/test_collection_constraints.py
@@ -4,110 +4,90 @@ import string
 import psycopg2
 
 
+def _test_constraint(test, coll, spec):
+    result = coll.add_constraints(spec['constraints'])
+    test.assertEqual(result, True)
+
+    result = coll.add_constraints(spec['constraints'])
+    test.assertEqual(result, False)
+
+    for data, expected_result in spec['docs']:
+        test._test_save(coll, data, expected_result)
+
+
 class TestAddRequiredConstraint(testutils.BedquiltTestCase):
 
     def test_add_simple_required_constraint(self):
         client = self._get_test_client()
         coll = client['people']
 
-        # add the constraint
-        result = coll.add_constraints({
-            'name': {'$required': 1}
-        })
-        self.assertEqual(result, True)
-
-        # add the constraint again should return false
-        result = coll.add_constraints({
-            'name': {'$required': 1}
-        })
-        self.assertEqual(result, False)
-
-        # reject bad doc
-        self._test_save(
-            coll,
-            {'_id': 'paul@example.com',
-             'age': 20},
-            psycopg2.IntegrityError
-        )
-        self.conn.rollback()
-
-        # accept doc with name present and null
-        self.assertEquals(
-            coll.save({
-                '_id': 'paul@example.com',
-                'name': None,
-                'age': 20
-            }),
-            'paul@example.com'
-        )
-
-        # accept doc with name present and set to a string value
-        self.assertEquals(
-            coll.save({
-                '_id': 'paul@example.com',
-                'name': 'Paul',
-                'age': 20
-            }),
-            'paul@example.com'
-        )
+        spec = {
+            'constraints': {'name': {'$required': 1}},
+            'docs': [
+                ({
+                    '_id': 'paul@example.com',
+                    'age': 20
+                 },
+                 psycopg2.IntegrityError),
+                ({
+                    '_id': 'paul@example.com',
+                    'name': None,
+                    'age': 20
+                 },
+                 'paul@example.com'),
+                ({
+                    '_id': 'paul@example.com',
+                    'name': 'Paul',
+                    'age': 20
+                 },
+                 'paul@example.com'),
+            ]
+        }
+        _test_constraint(self, coll, spec)
 
     def test_required_at_nested_path(self):
         client = self._get_test_client()
         coll = client['people']
 
-        result = coll.add_constraints({
-            'address.city': {'$required': 1}
-        })
-        self.assertEquals(True, result)
-
-        # reject doc without nested structure
-        self._test_save(
-            coll,
-            {'_id': 'paul@example.com',
-             'age': 20},
-            psycopg2.IntegrityError
-        )
-        self.conn.rollback()
-
-        # reject doc where address.city is not set
-        self._test_save(
-            coll,
-            {'_id': 'paul@example.com',
-             'age': 20,
-             'address': {
-                 'street': 'wat'
-             }},
-            psycopg2.IntegrityError
-        )
-        self.conn.rollback()
-
-        # accept doc with address.city present and null
-        self.assertEquals(
-            coll.save({
-                '_id': 'paul@example.com',
-                'name': None,
-                'age': 20,
-                'address': {
-                    'street': 'wat',
-                    'city': None
-                }
-            }),
-            'paul@example.com'
-        )
-
-        # accept doc with name present and set to a string value
-        self.assertEquals(
-            coll.save({
-                '_id': 'paul@example.com',
-                'name': 'Paul',
-                'age': 20,
-                'address': {
-                    'street': 'wat',
-                    'city': 'London'
-                }
-            }),
-            'paul@example.com'
-        )
+        spec = {
+            'constraints': {'address.city': {'$required': 1}},
+            'docs': [
+                ({
+                    '_id': 'paul@example.com',
+                    'age': 20
+                 },
+                 psycopg2.IntegrityError),
+                ({
+                    '_id': 'paul@example.com',
+                    'age': 20,
+                    'address': {
+                        'street': 'wat'
+                    }
+                 },
+                 psycopg2.IntegrityError),
+                ({
+                    '_id': 'paul@example.com',
+                    'name': None,
+                    'age': 20,
+                    'address': {
+                        'street': 'wat',
+                        'city': None
+                    }
+                 },
+                 'paul@example.com'),
+                ({
+                    '_id': 'paul@example.com',
+                    'name': 'Paul',
+                    'age': 20,
+                    'address': {
+                        'street': 'wat',
+                        'city': 'London'
+                    }
+                 },
+                 'paul@example.com')
+            ]
+        }
+        _test_constraint(self, coll, spec)
 
     def test_required_at_nested_array_path(self):
         client = self._get_test_client()
