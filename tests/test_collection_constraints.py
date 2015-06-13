36d789f10dd7be80ec76e35db833c35b19478489
Shane Kilkelly <shane@kilkelly.me>
2015-06-13 09:06:45 +0100

    Test $required at nested array path.

--- tests/test_collection_constraints.py
+++ tests/test_collection_constraints.py
@@ -109,6 +109,73 @@ class TestAddRequiredConstraint(testutils.BedquiltTestCase):
             'paul@example.com'
         )
 
+    def test_required_at_nested_array_path(self):
+        client = self._get_test_client()
+        coll = client['people']
+
+        result = coll.add_constraints({
+            'addresses.0.city': {'$required': 1}
+        })
+        self.assertEquals(True, result)
+
+        # reject doc without nested structure
+        self._test_save(
+            coll,
+            {'_id': 'paul@example.com',
+             'age': 20},
+            psycopg2.IntegrityError
+        )
+        self.conn.rollback()
+
+        # reject doc where addresses is empty
+        self._test_save(
+            coll,
+            {'_id': 'paul@example.com',
+             'age': 20,
+             'addresses': []},
+            psycopg2.IntegrityError
+        )
+        self.conn.rollback()
+
+        # reject doc where addresses.0.city is not set
+        self._test_save(
+            coll,
+            {'_id': 'paul@example.com',
+             'age': 20,
+             'addresses': [
+                 {'street': 'wat'}
+             ]},
+            psycopg2.IntegrityError
+        )
+        self.conn.rollback()
+
+        # accept doc with address.city present and null
+        self.assertEquals(
+            coll.save({
+                '_id': 'paul@example.com',
+                'name': None,
+                'age': 20,
+                'addresses': [
+                    {'street': 'wat',
+                     'city': None}
+                ]
+            }),
+            'paul@example.com'
+        )
+
+        # accept doc with name present and set to a string value
+        self.assertEquals(
+            coll.save({
+                '_id': 'paul@example.com',
+                'name': 'Paul',
+                'age': 20,
+                'addresses': [
+                    {'street': 'wat',
+                     'city': 'London'}
+                ]
+            }),
+            'paul@example.com'
+        )
 
 
     def test_removing_required_constraint(self):
