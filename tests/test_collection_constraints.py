27dae6f24bb317d95337b75e1fd78f5f92fb8238
Shane Kilkelly <shane@kilkelly.me>
2015-06-14 13:44:08 +0100

    Test notnull on nested path.

--- tests/test_collection_constraints.py
+++ tests/test_collection_constraints.py
@@ -211,3 +211,47 @@ class TestNotNullConstraint(testutils.BedquiltTestCase):
         }
 
         _test_constraint(self, coll, spec)
+
+    def test_notnull_on_nested_path(self):
+        client = self._get_test_client()
+        coll = client['people']
+
+        spec = {
+            'constraints': {'address.city': {'$notnull': 1}},
+            'tests': [
+                ({
+                    '_id': 'paul@example.com',
+                    'name': None
+                 },
+                 'paul@example.com'),
+                ({
+                    '_id': 'paul@example.com',
+                    'name': 'paul',
+                    'address': {
+                        'street': 'wat'
+                    }
+                 },
+                 'paul@example.com'),
+                ({
+                    '_id': 'paul@example.com',
+                    'name': 'paul',
+                    'address': {
+                        'street': 'wat',
+                        'city': 'here'
+                    }
+                 },
+                 'paul@example.com'),
+                ({
+                    '_id': 'paul@example.com',
+                    'name': 'paul',
+                    'address': {
+                        'street': 'wat',
+                        'city': None
+
+                    }
+                 },
+                 psycopg2.IntegrityError),
+            ]
+        }
+
+        _test_constraint(self, coll, spec)
